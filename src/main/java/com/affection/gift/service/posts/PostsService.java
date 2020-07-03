92ac263ce6264a2aedb61b2fe2546dc8c86dab58
cino4615 <cino4615@gmail.com>
2020-07-03 13:05:14 +0900

    application실행 전입니다

--- src/main/java/com/affection/gift/service/posts/PostsService.java
+++ src/main/java/com/affection/gift/service/posts/PostsService.java
@@ -1,10 +1,15 @@
 package com.affection.gift.service.posts;
 
+import com.affection.gift.domain.posts.Posts;
 import com.affection.gift.domain.posts.PostsRepository;
+import com.affection.gift.web.dto.PostsResponseDto;
 import com.affection.gift.web.dto.PostsSaveRequestDto;
+import com.affection.gift.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 //@RequiredArgsConstructor
 //final이 붙은 모든 필드를 인자값으로 하는 생성자를 롬복이 대신 생성해줌
@@ -15,5 +20,21 @@ public class PostsService {
 
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                        .orElseThrow(()->
+                             new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+                        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()->
+                new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
--- src/main/java/com/affection/gift/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/affection/gift/web/dto/PostsSaveRequestDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+//결과값으로 여러 테이블을 조인해서 줘야할 경우가 빈번하므로 Entity클래스 만으로 표현하기 어려움
 @Getter
 @NoArgsConstructor
 public class PostsSaveRequestDto {
