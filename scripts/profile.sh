e2e12e1d4b2e9396768470c9a9e1d77836ae5f54
cino4615 <cino4615@gmail.com>
2020-07-14 11:39:02 +0900

    포트 변경되나 확인

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,8 +1,9 @@
-#!/urs/bin/env bash
+#!/usr/bin/env bash
 
 # 쉬고 있는 profile 찾기 : real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 
-function find_idle_profile(){
+function find_idle_profile()
+{
     #현재 엔진엑스가 바라보고 있는 스플이 부트가 정상적으로 수행중인지 확인
     #응답값을 HttpStatus로 받습니다
     #정상이면 200, 오류가 발생하면 400~503 사이로 발생하니 400이상은 모두 예외로 보고
