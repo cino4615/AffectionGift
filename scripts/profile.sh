c9528504c5990279e304c7212175563aa7724abf
cino4615 <cino4615@gmail.com>
2020-07-20 15:43:52 +0900

    고치는중

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -17,7 +17,7 @@ function find_idle_profile()
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
-    if [[ ${CURRENT_PROFILE} == real1 ]]
+    if [ ${CURRENT_PROFILE} == real1 ]
     then
       IDLE_PROFILE=real2
     else
@@ -35,7 +35,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [[ ${IDLE_PROFILE} == real1 ]]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
